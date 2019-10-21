fce7fe4feb5f160b5bf806427e7959e1503ad62d
jivoy1988 <jivoy1988@gmail.com>
2019-10-21 12:43:37 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,19 +17,39 @@ then
 fi
 
 echo "$INPUT_KEY" > "$SSHPATH/deploy_key"
+
 chmod 700 "$SSHPATH"
 chmod 600 "$SSHPATH/known_hosts"
 chmod 600 "$SSHPATH/deploy_key"
 
 eval $(ssh-agent)
+
 ssh-add "$SSHPATH/deploy_key"
 
-#ssh-keyscan -t rsa $HOST >> "$SSHPATH/known_hosts"
+echo '' > $HOME/shell.sh
 
-echo "$INPUT_COMMAND" > $HOME/shell.sh
+IFS='
+'
+for i in $INPUT_COMMAND
+do
+    echo $i
+    echo 'COMMAND=$(cat <<-END' >> $HOME/shell.sh
+    echo $i >> $HOME/shell.sh
+    echo 'END' >> $HOME/shell.sh
+    echo ')' >> $HOME/shell.sh
+    echo 'echo "\e[34mRun command:"' >> $HOME/shell.sh
+    echo 'echo $COMMAND' >> $HOME/shell.sh
+    echo 'tput sgr0' >> $HOME/shell.sh
+    echo 'echo ""' >> $HOME/shell.sh
+    echo $i >> $HOME/shell.sh
+    echo 'echo ""' >> $HOME/shell.sh
+done
+
+echo "\e[32mCommands"
+
 cat $HOME/shell.sh
 
-echo Start Run Command
+echo "\e[32mStart Run Commands"
 
 if [ "$INPUT_PASS" = "" ]
 then
